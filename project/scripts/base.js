76335c01c2cd1baf810995c303d52bfc60ecb7df
ablaskey <allison.blaskey@outlook.com>
2025-07-30 11:28:07 -0700

    commit siteplan

---
+++ project/scripts/base.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("currentYear").textContent = new Date().getFullYear();
+    document.getElementById("lastModified").textContent = `Last modified: ${document.lastModified}`;
+});
+
+const menuButton = document.getElementById("menuBtn");
+const navMenu = document.getElementById("navMenu");
+
+menuButton.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+
+    const isOpen = navMenu.classList.contains("show");
+    menuButton.textContent = isOpen ? "❌" : "☰";
+});
